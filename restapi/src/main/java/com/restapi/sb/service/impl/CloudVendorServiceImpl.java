96a2521eb8711ef6b85481d6a825c27589f15bf5
Your Name <saisriman1771@gmail.com>
2024-09-30 11:02:07 -0500

    Initial Commit

---
+++ restapi/src/main/java/com/restapi/sb/service/impl/CloudVendorServiceImpl.java
@@ -0,0 +1,62 @@
+package com.restapi.sb.service.impl;
+
+
+import org.springframework.stereotype.Service;
+
+import com.restapi.sb.exception.CloudVendorNotFoundException;
+import com.restapi.sb.model.CloudVendor;
+import com.restapi.sb.repository.CloudVendorRepository;
+import com.restapi.sb.service.CloudVendorService;
+
+import java.util.List;
+
+@Service
+public class CloudVendorServiceImpl implements CloudVendorService {
+
+    CloudVendorRepository cloudVendorRepository;
+
+    public CloudVendorServiceImpl(CloudVendorRepository cloudVendorRepository) {
+        this.cloudVendorRepository = cloudVendorRepository;
+    }
+
+    @Override
+    public String createCloudVendor(CloudVendor cloudVendor) {
+        // More Business Logic
+        cloudVendorRepository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String updateCloudVendor(CloudVendor cloudVendor) {
+        // More Business Logic
+        cloudVendorRepository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String deleteCloudVendor(String cloudVendorId) {
+        // More Business Logic
+        cloudVendorRepository.deleteById(cloudVendorId);
+        return "Success";
+    }
+
+    @Override
+    public CloudVendor getCloudVendor(String cloudVendorId) {
+        // More Business Logic
+        if(cloudVendorRepository.findById(cloudVendorId).isEmpty())
+            throw new CloudVendorNotFoundException("Requested Cloud Vendor does not exist");
+        return cloudVendorRepository.findById(cloudVendorId).get();
+    }
+
+    @Override
+    public List<CloudVendor> getAllCloudVendors() {
+        // More Business Logic
+        return cloudVendorRepository.findAll();
+    }
+
+    @Override
+    public List<CloudVendor> getByVendorName(String vendorName)
+    {
+        return cloudVendorRepository.findByVendorName(vendorName);
+    }
+}
